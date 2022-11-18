1dba65108b003b0b2f615d813b93e3fa04be19ed
Nidhinsha <nidhinsha64@gmail.com>
2022-11-18 10:47:08 +0530

    atlas env

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ const state = {
 }
 module.exports.connect = function (done) {
     // const url='mongodb://localhost:27017'
-    const url = 'mongodb+srv://nidhinshavs:0tfUs9oKIqwZSjw0@cluster0.t1py4ir.mongodb.net/?retryWrites=true&w=majority'
+    const url = process.env.ATLAS
     const dbname = 'ecommerce'
     MongoClient.connect(url, (err, data) => {
         if (err) return done(err)
